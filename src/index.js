8a4f8371e018c060fbecd9b887d101c4e42bef15
mihairaevschi001 <mihairaevschi001@gmail.com>
2022-06-30 18:00:04 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { createRoot } from 'react-dom/client';
 import { UserProvider } from './context/user.context';
 import { ProductsProvider } from './context/products.context';
 import { CartProvider } from './context/cart.context';
-
+//1
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
